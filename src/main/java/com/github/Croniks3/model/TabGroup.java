adc10a56d3f1fc47a3ce2cd25c446b70708ad891
DESKTOP-E7L2H89\Kroniks <wowmandarin@yandex.ru>
2026-04-16 03:33:24 +0300

    created a class for the tab group

---
+++ src/main/java/com/github/Croniks3/model/TabGroup.java
@@ -0,0 +1,35 @@
+package com.github.Croniks3.model;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.List;
+import java.util.Objects;
+
+public final class TabGroup {
+    private final TabGroupDefinition definition;
+    private final List<String> filePaths;
+
+    public TabGroup(
+            @NotNull TabGroupDefinition definition,
+            @NotNull List<String> filePaths
+    ) {
+        this.definition = Objects.requireNonNull(definition);
+        this.filePaths = List.copyOf(Objects.requireNonNull(filePaths));
+    }
+
+    public @NotNull TabGroupDefinition getDefinition() {
+        return definition;
+    }
+
+    public @NotNull List<String> getFilePaths() {
+        return filePaths;
+    }
+
+    public int getFilesCount() {
+        return filePaths.size();
+    }
+
+    public boolean isEmpty() {
+        return filePaths.isEmpty();
+    }
+}
